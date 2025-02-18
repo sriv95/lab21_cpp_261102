8431353daca5b7b9cc531c0292829ecad45e94b6
sriv95 <srivichai95@gmail.com>
2025-02-18 14:37:34 +0700

    average.cpp complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+    if(argc>1){
+        cout<<"---------------------------------\n";
+        double sum=0;
+        for(int i=1;i<argc;i++){
+            sum += atof(argv[i]);
+        }
+        
+        cout<<"Average of "<<argc-1<<" numbers = "<<sum/(argc-1)<<endl;
+        cout<<"---------------------------------";
+    }
+    else cout<<"Please input numbers to find average.";
+}
